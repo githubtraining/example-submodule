83eeda1b304ef559f0d682bef71026c7d5d0894c
Tony Tarrant <training@github.com>
2013-08-15 13:34:25 -0600

    Add basic app JS startup functions

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+IIFE - Immediately Invoked Function Expression
+Encapsulate initialization of application.
+*/
+
+(function(){
+    var a = 0,
+        b = 1,
+        c = 2;
+
+    var startup = function(){
+        say("Welcome");
+    }
+    var say = function(msg){
+        alert(msg);
+    }
+    var shutdown = function(){
+        alert("Goodbye");
+    }
+
+    startup();
+});
